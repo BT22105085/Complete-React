33bdd63feeb26388264d8fbabb2da5cb1d53f6fb
BT22105085 <118710317+BT22105085@users.noreply.github.com>
2024-06-26 19:44:46 +0530

    Going to start

---
+++ 2 create-react-comp/create-react-comp/src/App.jsx
@@ -0,0 +1,12 @@
+import Pkbutton from "./Pkbutton.jsx";
+
+function App(){
+  return <div>
+    <h1>
+    Hello world!
+    Prashant Here!
+  </h1>
+  <Pkbutton/>
+  </div>
+}
+export default App;
---
+++ 2 create-react-comp/create-react-comp/src/Pkbutton.jsx
@@ -0,0 +1,9 @@
+function Pkbutton(){
+  return <div>
+    <button style={{"backgroundColor":"antiquewhite"}}>
+    Kindly hit here
+    </button>
+    <p>I'm a para</p>
+  </div>;
+}
+export default Pkbutton;
